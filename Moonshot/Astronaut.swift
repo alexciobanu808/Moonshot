af79fadde72e087377c54d2fbd9f9fe55c3b7cf8
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2022-02-11 12:18:55 -0500

    First Stage

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Alex Ciobanu on 2/10/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
